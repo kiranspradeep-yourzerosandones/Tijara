3929050194d0996c0e68b83ea299629fac132960
kiranspradeep-yourzerosandones <kiranspradeep@yourzerosandones.com>
2026-07-27 16:57:02 +0530

    final

--- mobile-app/src/config/env.production.js
+++ mobile-app/src/config/env.production.js
@@ -1,8 +1,8 @@
 // src/config/env.production.js
 const PROD_ENV = {
-  API_URL: 'https://api.tijara.com/api',
-  IMAGE_BASE_URL: 'https://api.tijara.com',
-  SOCKET_URL: 'https://api.tijara.com',
+  API_URL: 'https://admin.tijaracorp.com/api',
+  IMAGE_BASE_URL: 'https://admin.tijaracorp.com',
+  SOCKET_URL: 'https://admin.tijaracorp.com',
   DEBUG: false,
   LOG_LEVEL: 'error',
   ENVIRONMENT: 'production',
